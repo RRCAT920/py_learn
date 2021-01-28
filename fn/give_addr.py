61dcb14ac8723bcb5851664ef7232728e611333d
胡子豪 <wuqieor@outlook.com>
2021-01-28 18:51:22 +0800

    给函数传递列表

---
+++ fn/give_addr.py
@@ -0,0 +1,25 @@
+"""
+给函数传递列表
+"""
+
+
+names = ["alex", "jack"]
+
+
+def change(lst):
+    lst.append("harry")
+
+
+change(names)
+print(names)
+
+
+name = "harrison"
+
+
+def change_str(string):
+    string = "marry"
+
+
+change_str(name)
+print(name)
