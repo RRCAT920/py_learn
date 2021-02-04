67ae0aaf9d5bbda48ce5cf08be152c441adc3079
胡子豪 <wuqieor@outlook.com>
2021-02-04 17:51:10 +0800

    带MD5的用户验证

---
+++ account/validation.py
@@ -0,0 +1,29 @@
+import json
+from datetime import datetime
+import hashlib
+
+file = ''
+info = None
+for _ in range(3):
+    uin = input('Username: '), input('Password: ')
+
+    file = f'{uin[0]}.json'
+    with open(file, 'r+') as fio:
+        info = json.load(fio)
+        password = info['password']
+
+        if hashlib.md5(uin[1].encode()).hexdigest() == password:
+            if info['status']:
+                print('用户已锁定')
+                break
+            if datetime.today() > datetime.strptime(info['expire_date'],
+                                                    '%Y-%m-%d'):
+                print('用户已过期')
+                break
+
+            print('登录成功')
+            break
+else:
+    info['status'] = 1
+    with open(file, 'w') as fout:
+        json.dump(info, fout)
--- qqsender.py
+++ qqsender.py
@@ -1,48 +1,81 @@
-from pynput.mouse import Controller as Mouse
-from pynput.mouse import Button as bt
-from pynput.keyboard import Controller as Keyboard
-from pynput.keyboard import Key
+import tkinter
+from tkinter import *
+from tkinter import ttk
+import win32gui
+import win32con
+import win32clipboard
 
 
-def changeWindow(kb):
-    kb.press(Key.cmd)
-    kb.press(Key.tab)
-    kb.release(Key.cmd)
-    kb.release(Key.tab)
+root = tkinter.Tk()
+root.title("massage bomb v0.1")
+Msg = StringVar()
+Name = StringVar()
 
 
-def TellHer(ms, kb):
-    # ms.position = ms.
-    ms.press(bt.left)
-    ms.release(bt.left)
-    kb.type("nimenjidianke")
-    kb.press(Key.space)
-    kb.release(Key.space)
-    # kb.press(Key.shift)
-    # kb.release(Key.shift)
-    kb.press(Key.space)
-    kb.release(Key.space)
-    kb.type("nimenjidianke")
-    kb.press(Key.space)
-    kb.release(Key.space)
-    kb.press(Key.space)
-    kb.release(Key.space)
-    kb.press(Key.enter)
-    kb.release(Key.enter)
-    # kb.press(Key.shift)
-    # kb.release(Key.shift)
-    # time.sleep(0.3)
+def send_message():
+    msg = Msg.get()
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32con.CF_UNICODETEXT,msg)
+    win32clipboard.CloseClipboard()
+
+    name_win = Name.get()
+    handle = win32gui.FindWindow('TXGuiFoundation', name_win)
+    # title = win32gui.GetWindowText(handle)
+    # clsname = win32gui.GetClassName(handle)
+    # childList = []
+    # win32gui.EnumChildWindows(handle, lambda hwnd, param: param.append(hwnd), childList)
+    # for i in childList:
+    #     listwin.insert(i)
+
+    n = 10
+    while n != 0:
+        win32gui.SendMessage(handle, 770, 0, 0)
+        win32gui.SendMessage(handle, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+        n = n-1
 
 
-def main():
-    ms = Mouse()
-    kb = Keyboard()
-    changeWindow(kb)
-    for i in range(20):
-        TellHer(ms, kb)
-        # kb.type("^v")
-        # kb.press(Key.cmd)
+content = ttk.Frame(root, padding=(3,3,12,12))
+frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100)
+
+listwin = Listbox(frame)
+
+sendbl = ttk.Label(content, text="Message:", font=('Arial', 10))
+message = ttk.Entry(content, textvariable=Msg)
+
+namebl = ttk.Label(content, text="name:", font=('Arial', 10))
+name = ttk.Entry(content, textvariable=Name)
+
+notice = ttk.Label(content, text="运行时注意：\n只保持一个QQ对话窗\n输入对象备注名和要发送的信息")
+
+send = ttk.Button(content, text="Send", command=send_message)
+stop = ttk.Button(content, text='Exit', command=root.quit)
+
+content.grid(column=0, row=0, sticky=(N, S, E, W))
+frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+
+listwin.grid(column=0, row=0, sticky=(N, S, E, W))
+
+namebl.grid(column=3, row=0,  sticky=(N, W), padx=5)
+name.grid(column=3, row=1,  sticky=(N, W), pady=5, padx=5)
+
+sendbl.grid(column=4, row=0, columnspan=2, sticky=(N, W), padx=5)
+message.grid(column=4, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
+
+notice.grid(column=3, row=1)
+
+send.grid(column=3, row=3)
+stop.grid(column=4, row=3)
 
 
-if __name__ == '__main__':
-    main()
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+content.columnconfigure(0, weight=3)
+content.columnconfigure(1, weight=3)
+content.columnconfigure(2, weight=3)
+content.columnconfigure(3, weight=1)
+content.columnconfigure(4, weight=1)
+content.rowconfigure(1, weight=1)
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+root.mainloop()
